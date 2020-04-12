6500cf8fc0dc181d6e4e120fd672bfd582198bd5
Hugo Landau <hlandau@devever.net>
2020-04-12 02:36:15 +0100

    Remove redundant code from specwriter-xml

--- src/specwriter-xml.scm
+++ src/specwriter-xml.scm
@@ -1,11 +1,4 @@
 (define-module (specwriter-xml))
 (use-modules (ice-9 match) (sxml2))
 
-;; sxml-document -> sxml-top
-(define (sw-xml-top document)
-  `(*TOP*
-     (*PI* xml-stylesheet "href=\"schspec-xml.css\" type=\"text/css\"")
-     (top (@ (@ (*NAMESPACES* (*IMPLICIT* "https://www.devever.net/ns/schspec-iso" *IMPLICIT*))))
-          ,document)))
-
-(export sw-xml-top)
+(export)
